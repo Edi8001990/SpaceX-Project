43a9ae82eff728d157afd07af7d2090447e5c13a
Mateusz <mateusz.trajdos@gmail.com>
2019-06-09 13:58:39 +0100

    Done

--- client/src/views/launch_view.js
+++ client/src/views/launch_view.js
@@ -14,11 +14,34 @@ LaunchView.prototype.bindEvents = function() {
 }
 
 LaunchView.prototype.render = function(launch) {
+
   const mission_name = this.createElement('h3',"Mission name: " + launch.mission_name);
   this.container.appendChild(mission_name)
 
-  const launch_success = this.createElement('p',"Lauch success: " +  launch.launch_success)
+  const mission_details = this.createElement('p', "Mission Details: " + launch.details)
+  this.container.appendChild(mission_details)
+
+  const launch_success = this.createElement('p', "Lauch success: " +  launch.launch_success)
   this.container.appendChild(launch_success)
+
+
+  if (launch.launch_success === false){
+
+  const launch_failure_reason = this.createElement('p', "Launch failure details: " + launch.launch_failure_details.reason)
+  this.container.appendChild(launch_failure_reason)
+
+  const launch_failure_time_since_start = this.createElement('p', "Launch Failure Time Since Start: " + launch.launch_failure_details.time + "s")
+  this.container.appendChild(launch_failure_time_since_start)
+
+  if (launch.launch_failure_details.altitude != null){
+  const launch_failure_altitude = this.createElement('p', "Launch failure altitude: " + launch.launch_failure_details.altitude + "000m")
+  this.container.appendChild(launch_failure_altitude)}
+
+  else{
+    const launch_failure_altitude = this.createElement('p', "Launch failure altitude: " + " 0m")
+    this.container.appendChild(launch_failure_altitude);
+  }
+  }
 
   const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"];
@@ -30,9 +53,24 @@ LaunchView.prototype.render = function(launch) {
   const launch_date = this.createElement('p', "Launch date: " + lauchFormattedDate)
   this.container.appendChild(launch_date)
 
+  const rocket_name = this.createElement('p', "Rocket Name: " +  launch.rocket.rocket_name)
+  this.container.appendChild(rocket_name)
+
+  const rocket_type = this.createElement('p', "Rocket Type: " +  launch.rocket.rocket_type)
+  this.container.appendChild(rocket_type)
+
   const launchImage = document.createElement('img')
   launchImage.src = launch.links.mission_patch
+  launchImage.classList.add('launch-logo')
   this.container.appendChild(launchImage)
+
+  const launchVideo = document.createElement('iframe')
+  launchVideo.src = "https://www.youtube.com/embed/" + launch.links.youtube_id
+  launchVideo.classList.add('launch-yt-video')
+  launchVideo.style.width = "560px";
+  launchVideo.style.height = "315px";
+  this.container.appendChild(launchVideo)
+
 
 }
 
